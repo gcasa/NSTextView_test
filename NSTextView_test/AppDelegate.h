776020b8252bfd8eae41e2d288caaf50a3bdaee7
Gregory Casamento <greg.casamento@gmail.com>
2020-09-11 13:16:00 -0400

    Initial Commit

---
+++ NSTextView_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSTextView_test
+//
+//  Created by Gregory Casamento on 9/11/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
